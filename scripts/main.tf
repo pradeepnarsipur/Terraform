b2b0ca50a89b25888667c4e38b08559352d4110a
pradeep <pradeepnarsipur@gmail.com>
2022-03-19 17:19:52 +0530

    commit

--- scripts/main.tf
+++ scripts/main.tf
@@ -1,72 +1,35 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-resources"
+resource "azurerm_resource_group" "pradeep_example_rg" {
+  name     = "azure-functions-test-rg"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_storage_account" "pradeep_storaage_example" {
+  name                     = "functions_app_testsa_1989"
+  resource_group_name      = azurerm_resource_group.pradeep_example_rg.name
+  location                 = azurerm_resource_group.pradeep_example_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+resource "azurerm_app_service_plan" "pradeep_example_app_service_plan" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.pradeep_example_rg.location
+  resource_group_name = azurerm_resource_group.pradeep_example_rg.name
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
+  sku {
+    tier = "Standard"
+    size = "S1"
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
+resource "azurerm_function_app" "example" {
+  name                       = "test-azure-functions_pradeep"
+  location                   = azurerm_resource_group.pradeep_example_rg.location
+  resource_group_name        = azurerm_resource_group.pradeep_example_rg.name
+  app_service_plan_id        = azurerm_app_service_plan.pradeep_example_app_service_plan.id
+  storage_account_name       = azurerm_storage_account.pradeep_storaage_example.name
+  storage_account_access_key = azurerm_storage_account.pradeep_storaage_example.primary_access_key
+  site_config {
+    dotnet_framework_version="v6.0"
   }
 }
